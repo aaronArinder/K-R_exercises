38eb0beb3481480db034e7ea39084893d326024e
Aaron Arinder <aaronarinder@gmail.com>
2019-11-01 08:22:56 -0400

    Ch 3: added get_print_line.c

---
+++ ch_3/get_print_line.c
@@ -0,0 +1,34 @@
+/*
+ * ex: #include get_print_lin.c
+ *
+ * my_get_line:
+ * print_line:
+ * */
+
+#include <stdio.h>
+// named as such because there's already a get_line on my system
+void my_get_line (char s[], int lim);
+void print_line (char line[]);
+
+void my_get_line (char s[], int lim) {
+  int c, i;
+  for (i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+    s[i] = c;
+  }
+  s[i] = '\0';
+  return;
+}
+
+void print_line (char line[]) {
+  int stop = 0;
+
+  for (int l = 0; stop == 0; ++l) {
+    if (line[l] > '\0') {
+      printf("%c", line[l]);
+    } else {
+      stop = 1;
+    }
+  }
+  return;
+}
+
